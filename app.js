9be90da39a064f352199f7bb66f602829aa77dd6
Kifeb <rizkyfebrianto10@gmail.com>
2022-07-01 20:33:02 +0700

    menambah file app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+
+//Run Server
+app.listen(port, () => {
+    console.log(`Server Run On http://localhost:${port}`)
+})
+
+
+
+
+
+
