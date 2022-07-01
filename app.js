6365a989bc40b2926c1efac2017816e359f90d7e
Kifeb <rizkyfebrianto10@gmail.com>
2022-07-01 21:04:51 +0700

    Mengambil file di idstack

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+require('./src/routes/home.routes')(app)
 
 //Run Server
 app.listen(port, () => {
     console.log(`Server Run On http://localhost:${port}`)
-})
+});
 
 
 
