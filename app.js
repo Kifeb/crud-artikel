832209d94cbcce51c383c703eb6186ed54d1492a
Kifeb <rizkyfebrianto10@gmail.com>
2022-07-01 21:25:03 +0700

    templateing dengan ejs

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const port = 5000;
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-require('./src/routes/home.routes')(app)
+//Konfigurasi File Static
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+require('./src/routes/home.routes')(app);
 
 //Run Server
 app.listen(port, () => {
