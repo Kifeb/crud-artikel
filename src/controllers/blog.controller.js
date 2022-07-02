e9ca3f7d418f4109993e29cec2acb051ea7f472e
Kifeb <rizkyfebrianto10@gmail.com>
2022-07-02 07:49:31 +0700

    binding data dari partials blog

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -16,3 +16,7 @@ exports.store = async (req, res) => {
         res.redirect('/blog/create');
     };
 };
+
+exports.show = (req, res) => {
+    res.render('blog/show');
+}
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -3,7 +3,8 @@ module.exports = (app) => {
     const router = require('express').Router();
 
     router.get('/create', blog.create);
-    router.post('/', blog.store)
+    router.post('/', blog.store);
+    router.get('/:id', blog.show)
 
     app.use('/blog', router)
 };
