57338f36c0c2762f0534d185104fe61f0c71e53f
Kifeb <rizkyfebrianto10@gmail.com>
2022-07-02 08:02:05 +0700

    menampilakan data by id dari db

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -17,6 +17,15 @@ exports.store = async (req, res) => {
     };
 };
 
-exports.show = (req, res) => {
-    res.render('blog/show');
-}
+exports.show = async (req, res) => {
+    try {
+        const id = req.params.id;
+        let article = await Article.findById(id);
+        res.render('blog/show', {
+            article,
+        });
+    } catch (e) {
+        res.redirect('/')
+    };
+
+};
